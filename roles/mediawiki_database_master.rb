f529247d166c22ea8c1800881c71a1f64f0b9550
Seth Chisamore <schisamo@opscode.com>
2011-04-11 07:25:11 -0500

    fix content of database master roles

--- roles/mediawiki_database_master.rb
+++ roles/mediawiki_database_master.rb
@@ -1,7 +1,5 @@
 name "mediawiki_database_master"
 description "database master for the mediawiki application."
 run_list(
-  "recipe[mysql::client]",
-  "recipe[application]",
-  "recipe[mediawiki]"
+  "recipe[database::master]"
 )
